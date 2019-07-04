3ed1a5f49a092e217b70b2df43b512b43114c074
Tomas Tauber <2410580+tomtau@users.noreply.github.com>
2019-07-04 15:52:14 +0800

    Problem: no ZMQ server code in enclave app wrapper (CRO-201)
    Solution: added a basic ZMQ server listening code + better logging

--- app/src/main.rs
+++ app/src/main.rs
@@ -3,26 +3,32 @@ mod server;
 
 use crate::enclave_u::init_enclave::init_enclave;
 use crate::server::TxValidationServer;
+use log::{error, info};
 use std::env;
 use std::thread;
 
 fn main() {
+    env_logger::init();
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        error!("Please provide the ZMQ connection string (e.g. \"tcp://127.0.0.1:25933\") as the first argument");
+        return;
+    }
     let enclave = match init_enclave() {
         Ok(r) => {
-            println!("[+] Init Enclave Successful {}!", r.geteid());
+            info!("[+] Init Enclave Successful {}!", r.geteid());
             r
         }
         Err(x) => {
-            println!("[-] Init Enclave Failed {}!", x.as_str());
+            error!("[-] Init Enclave Failed {}!", x.as_str());
             return;
         }
     };
 
-    let args: Vec<String> = env::args().collect();
-
     let child_t = thread::spawn(move || {
         let mut server =
             TxValidationServer::new(&args[1], enclave).expect("could not start a zmq server");
+        info!("starting zmq server");
         server.execute()
     });
     child_t.join().expect("server thread failed");
--- app/src/server/mod.rs
+++ app/src/server/mod.rs
@@ -1,8 +1,10 @@
-use crate::enclave_u::initchain;
+use crate::enclave_u::{check_initchain, check_transfertx};
+use chain_core::tx::TxAux;
+use enclave_protocol::{EnclaveRequest, EnclaveResponse, FLAGS};
+use log::{debug, info};
+use parity_codec::{Decode, Encode};
 use sgx_urts::SgxEnclave;
 use zmq::{Context, Error, Socket, REP};
-
-pub const FLAGS: i32 = 0;
 
 pub struct TxValidationServer {
     socket: Socket,
@@ -18,9 +20,49 @@ impl TxValidationServer {
     }
 
     pub fn execute(&mut self) {
+        info!("running zmq server");
         loop {
             if let Ok(msg) = self.socket.recv_bytes(FLAGS) {
-                unimplemented!("FIXME")
+                debug!("received a message");
+                let mcmd = EnclaveRequest::decode(&mut msg.as_slice());
+                let resp = match mcmd {
+                    Some(EnclaveRequest::CheckChain { chain_hex_id }) => {
+                        debug!("check chain");
+                        EnclaveResponse::CheckChain(check_initchain(
+                            self.enclave.geteid(),
+                            chain_hex_id,
+                        ))
+                    }
+                    Some(EnclaveRequest::VerifyTx {
+                        tx: TxAux::TransferTx(tx, witness),
+                        inputs,
+                        min_fee_computed,
+                        previous_block_time,
+                        unbonding_period,
+                    }) => {
+                        debug!("verify transfer tx");
+                        EnclaveResponse::VerifyTx(check_transfertx(
+                            self.enclave.geteid(),
+                            TxAux::TransferTx(tx, witness),
+                            inputs,
+                            min_fee_computed,
+                            previous_block_time,
+                            unbonding_period,
+                        ))
+                    }
+                    Some(_) => {
+                        debug!("verify other tx");
+                        EnclaveResponse::UnsupportedTxType
+                    }
+                    None => {
+                        debug!("unknown request / failed to decode");
+                        EnclaveResponse::UnsupportedTxType
+                    }
+                };
+                let response = resp.encode();
+                self.socket
+                    .send(response, FLAGS)
+                    .expect("reply sending failed");
             }
         }
     }

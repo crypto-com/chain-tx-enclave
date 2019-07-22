5667c4345715556e03455198e1974ecfc941d276
Tomas Tauber <2410580+tomtau@users.noreply.github.com>
2019-07-22 19:18:53 +0800

    Problem: (CRO-208) deposit bonded and withdraw unbounded aren't done in enclaves
    Solution: added a basic support in enclave and enclave app wrapper

--- app/src/enclave_u/mod.rs
+++ app/src/enclave_u/mod.rs
@@ -2,11 +2,12 @@ pub mod init_enclave;
 
 use sgx_types::*;
 
-use chain_core::common::Timespec;
 use chain_core::init::coin::Coin;
+use chain_core::state::account::DepositBondTx;
 use chain_core::state::account::StakedState;
 use chain_core::tx::fee::Fee;
 use chain_core::tx::TxAux;
+use chain_core::ChainInfo;
 use chain_tx_validation::TxWithOutputs;
 use parity_codec::Encode;
 
@@ -20,15 +21,39 @@ extern "C" {
     fn ecall_check_transfer_tx(
         eid: sgx_enclave_id_t,
         retval: *mut sgx_status_t,
-        min_computed_fee: u64,
         actual_fee_paid: *mut u64,
-        previous_block_time: i64,
-        unbonding_period: u32,
+        chain_info: *const u8,
+        chain_info_len: usize,
         txaux: *const u8,
         txaux_len: usize,
         txsin: *const u8,
         txsin_len: usize,
     ) -> sgx_status_t;
+
+    fn ecall_check_deposit_tx(
+        eid: sgx_enclave_id_t,
+        retval: *mut sgx_status_t,
+        input_coin_sum: *mut u64,
+        chain_info: *const u8,
+        chain_info_len: usize,
+        txaux: *const u8,
+        txaux_len: usize,
+        txsin: *const u8,
+        txsin_len: usize,
+    ) -> sgx_status_t;
+
+    fn ecall_check_withdraw_tx(
+        eid: sgx_enclave_id_t,
+        retval: *mut sgx_status_t,
+        actual_fee_paid: *mut u64,
+        chain_info: *const u8,
+        chain_info_len: usize,
+        txaux: *const u8,
+        txaux_len: usize,
+        account: *const u8,
+        account_len: usize,
+    ) -> sgx_status_t;
+
 }
 
 pub fn check_initchain(eid: sgx_enclave_id_t, chain_hex_id: u8) -> Result<(), ()> {
@@ -43,25 +68,24 @@ pub fn check_initchain(eid: sgx_enclave_id_t, chain_hex_id: u8) -> Result<(), ()
 
 pub fn check_transfertx(
     eid: sgx_enclave_id_t,
-    tx_enc: Vec<u8>,
+    txaux: TxAux,
     txins: Vec<TxWithOutputs>,
-    min_computed_fee: Fee,
-    previous_block_time: Timespec,
-    unbonding_period: u32,
+    info: ChainInfo,
 ) -> Result<(Fee, Option<StakedState>), ()> {
     let txins_enc: Vec<u8> = txins.encode();
+    let txaux_enc: Vec<u8> = txaux.encode();
+    let info_enc: Vec<u8> = info.encode();
     let mut retval: sgx_status_t = sgx_status_t::SGX_SUCCESS;
     let mut actual_fee_paid = 0;
     let result = unsafe {
         ecall_check_transfer_tx(
             eid,
             &mut retval,
-            min_computed_fee.to_coin().into(),
             &mut actual_fee_paid,
-            previous_block_time,
-            unbonding_period,
-            tx_enc.as_ptr(),
-            tx_enc.len(),
+            info_enc.as_ptr(),
+            info_enc.len(),
+            txaux_enc.as_ptr(),
+            txaux_enc.len(),
             txins_enc.as_ptr(),
             txins_enc.len(),
         )
@@ -71,6 +95,99 @@ pub fn check_transfertx(
             Coin::new(actual_fee_paid).expect("fee should not be larger than coin supply"),
         );
         Ok((fee, None))
+    } else {
+        Err(())
+    }
+}
+
+pub fn check_deposit_tx(
+    eid: sgx_enclave_id_t,
+    txaux: TxAux,
+    txins: Vec<TxWithOutputs>,
+    maccount: Option<StakedState>,
+    info: ChainInfo,
+) -> Result<(Fee, Option<StakedState>), ()> {
+    let txins_enc: Vec<u8> = txins.encode();
+    let txaux_enc: Vec<u8> = txaux.encode();
+    let info_enc: Vec<u8> = info.encode();
+    let mut retval: sgx_status_t = sgx_status_t::SGX_SUCCESS;
+    let mut input_coin_sum = 0;
+    let result = unsafe {
+        ecall_check_deposit_tx(
+            eid,
+            &mut retval,
+            &mut input_coin_sum,
+            info_enc.as_ptr(),
+            info_enc.len(),
+            txaux_enc.as_ptr(),
+            txaux_enc.len(),
+            txins_enc.as_ptr(),
+            txins_enc.len(),
+        )
+    };
+    if retval == sgx_status_t::SGX_SUCCESS && result == retval {
+        let deposit_amount = (Coin::new(input_coin_sum).expect("correct coin")
+            - info.min_fee_computed.to_coin())
+        .expect("init");
+        let account = match (maccount, txaux) {
+            (Some(mut a), _) => {
+                a.deposit(deposit_amount);
+                Some(a)
+            }
+            (
+                None,
+                TxAux::DepositStakeTx {
+                    tx:
+                        DepositBondTx {
+                            to_staked_account, ..
+                        },
+                    ..
+                },
+            ) => Some(StakedState::new_init(
+                deposit_amount,
+                info.previous_block_time,
+                to_staked_account,
+                true,
+            )),
+            (_, _) => unreachable!("one shouldn't call this with other variants"),
+        };
+        let fee = info.min_fee_computed;
+        Ok((fee, account))
+    } else {
+        Err(())
+    }
+}
+
+pub fn check_withdraw_tx(
+    eid: sgx_enclave_id_t,
+    txaux: TxAux,
+    mut account: StakedState,
+    info: ChainInfo,
+) -> Result<(Fee, Option<StakedState>), ()> {
+    let account_enc: Vec<u8> = account.encode();
+    let txaux_enc: Vec<u8> = txaux.encode();
+    let info_enc: Vec<u8> = info.encode();
+    let mut retval: sgx_status_t = sgx_status_t::SGX_SUCCESS;
+    let mut actual_fee_paid = 0;
+    let result = unsafe {
+        ecall_check_withdraw_tx(
+            eid,
+            &mut retval,
+            &mut actual_fee_paid,
+            info_enc.as_ptr(),
+            info_enc.len(),
+            txaux_enc.as_ptr(),
+            txaux_enc.len(),
+            account_enc.as_ptr(),
+            account_enc.len(),
+        )
+    };
+    if retval == sgx_status_t::SGX_SUCCESS && result == retval {
+        let fee = Fee::new(
+            Coin::new(actual_fee_paid).expect("fee should not be larger than coin supply"),
+        );
+        account.withdraw();
+        Ok((fee, Some(account)))
     } else {
         Err(())
     }
--- enclave/src/lib.rs
+++ enclave/src/lib.rs
@@ -8,10 +8,14 @@
 #[macro_use]
 extern crate sgx_tstd as std;
 
-use chain_core::init::coin::Coin;
-use chain_core::tx::fee::Fee;
-use chain_core::tx::PlainTxAux;
-use chain_tx_validation::{verify_transfer, ChainInfo, TxWithOutputs};
+use chain_core::state::account::StakedState;
+use chain_core::tx::TransactionId;
+use chain_core::tx::{data::input::TxoIndex, PlainTxAux, TxAux, TxObfuscated};
+use chain_tx_validation::witness::verify_tx_recover_address;
+use chain_tx_validation::{
+    verify_bonded_deposit_core, verify_transfer, verify_unbonded_withdraw_core, ChainInfo,
+    TxWithOutputs,
+};
 use enclave_macro::get_network_id;
 use parity_codec::Decode;
 use sgx_types::sgx_status_t;
@@ -30,47 +34,182 @@ pub extern "C" fn ecall_initchain(chain_hex_id: u8) -> sgx_status_t {
     }
 }
 
+#[inline]
+fn check_chain_info(chain_info: *const u8, chain_info_len: usize) -> Option<ChainInfo> {
+    let mut chain_info_slice = unsafe { slice::from_raw_parts(chain_info, chain_info_len) };
+    let chain_info = ChainInfo::decode(&mut chain_info_slice);
+    match chain_info {
+        Some(ChainInfo { chain_hex_id, .. }) if chain_hex_id != NETWORK_HEX_ID => None,
+        _ => chain_info,
+    }
+}
+
+/// FIXME: struct / typedef / fixed-size array for chain info
 #[no_mangle]
 pub extern "C" fn ecall_check_transfer_tx(
-    min_computed_fee: u64,
     actual_fee_paid: *mut u64,
-    previous_block_time: i64,
-    unbonding_period: u32,
+    chain_info: *const u8,
+    chain_info_len: usize,
     txaux: *const u8,
     txaux_len: usize,
     txsin: *const u8,
     txsin_len: usize,
 ) -> sgx_status_t {
-    let fee_c = Coin::new(min_computed_fee);
-    if fee_c.is_err() {
-        return sgx_status_t::SGX_ERROR_INVALID_PARAMETER;
-    }
-    let fee = Fee::new(fee_c.unwrap());
-    let mut txaux_slice = unsafe { slice::from_raw_parts(txaux, txaux_len) };
-    let mut inputs_slice = unsafe { slice::from_raw_parts(txsin, txsin_len) };
-    // FIXME: decrypting
-    let txaux = PlainTxAux::decode(&mut txaux_slice);
-    let inputs: Option<Vec<TxWithOutputs>> = Decode::decode(&mut inputs_slice);
+    let info = match check_chain_info(chain_info, chain_info_len) {
+        None => {
+            return sgx_status_t::SGX_ERROR_INVALID_PARAMETER;
+        }
+        Some(x) => x,
+    };
 
-    return match (txaux, inputs) {
-        (Some(PlainTxAux::TransferTx(tx, witness)), Some(input_txs)) => {
-            let info = ChainInfo {
-                min_fee_computed: fee,
-                chain_hex_id: NETWORK_HEX_ID,
-                previous_block_time,
-                unbonding_period,
-            };
-            let result = verify_transfer(&tx, &witness, info, input_txs);
-            if result.is_err() {
+    let mut txaux_slice = unsafe { slice::from_raw_parts(txaux, txaux_len) };
+    let txaux = TxAux::decode(&mut txaux_slice);
+    if let Some(TxAux::TransferTx {
+        txid,
+        payload: TxObfuscated { txpayload, .. },
+        no_of_outputs,
+        ..
+    }) = txaux
+    {
+        // FIXME: decrypting
+        let mut inputs_slice = unsafe { slice::from_raw_parts(txsin, txsin_len) };
+        let inputs: Option<Vec<TxWithOutputs>> = Decode::decode(&mut inputs_slice);
+        let plaintx = PlainTxAux::decode(&mut txpayload.as_slice());
+        match (plaintx, inputs) {
+            (Some(PlainTxAux::TransferTx(tx, witness)), Some(input_txs)) => {
+                if tx.id() != txid || tx.outputs.len() as TxoIndex != no_of_outputs {
+                    return sgx_status_t::SGX_ERROR_INVALID_PARAMETER;
+                }
+                let result = verify_transfer(&tx, &witness, info, input_txs);
+                if result.is_err() {
+                    return sgx_status_t::SGX_ERROR_INVALID_PARAMETER;
+                }
+                let actual_fee: u64 = result.unwrap().to_coin().into();
+                unsafe {
+                    *actual_fee_paid = actual_fee;
+                }
+                // FIXME: sealing
+                sgx_status_t::SGX_SUCCESS
+            }
+            _ => {
                 return sgx_status_t::SGX_ERROR_INVALID_PARAMETER;
             }
-            let actual_fee: u64 = result.unwrap().to_coin().into();
-            unsafe {
-                *actual_fee_paid = actual_fee;
-            }
-            // FIXME: sealing
-            sgx_status_t::SGX_SUCCESS
         }
-        _ => sgx_status_t::SGX_ERROR_INVALID_PARAMETER,
+    } else {
+        sgx_status_t::SGX_ERROR_INVALID_PARAMETER
+    }
+}
+
+/// FIXME: struct / typedef / fixed-size array for chain info
+#[no_mangle]
+pub extern "C" fn ecall_check_deposit_tx(
+    input_coin_sum: *mut u64,
+    chain_info: *const u8,
+    chain_info_len: usize,
+    txaux: *const u8,
+    txaux_len: usize,
+    txsin: *const u8,
+    txsin_len: usize,
+) -> sgx_status_t {
+    let info = match check_chain_info(chain_info, chain_info_len) {
+        None => {
+            return sgx_status_t::SGX_ERROR_INVALID_PARAMETER;
+        }
+        Some(x) => x,
     };
+
+    let mut txaux_slice = unsafe { slice::from_raw_parts(txaux, txaux_len) };
+    let txaux = TxAux::decode(&mut txaux_slice);
+    if let Some(TxAux::DepositStakeTx {
+        tx,
+        payload: TxObfuscated { txpayload, .. },
+    }) = txaux
+    {
+        // FIXME: decrypting
+        let mut inputs_slice = unsafe { slice::from_raw_parts(txsin, txsin_len) };
+        let inputs: Option<Vec<TxWithOutputs>> = Decode::decode(&mut inputs_slice);
+        let plaintx = PlainTxAux::decode(&mut txpayload.as_slice());
+        match (plaintx, inputs) {
+            (Some(PlainTxAux::DepositStakeTx(witness)), Some(input_txs)) => {
+                let result = verify_bonded_deposit_core(&tx, &witness, info, input_txs);
+                if result.is_err() {
+                    return sgx_status_t::SGX_ERROR_INVALID_PARAMETER;
+                }
+                let incoins: u64 = result.unwrap().into();
+                unsafe {
+                    *input_coin_sum = incoins;
+                }
+                sgx_status_t::SGX_SUCCESS
+            }
+            _ => {
+                return sgx_status_t::SGX_ERROR_INVALID_PARAMETER;
+            }
+        }
+    } else {
+        sgx_status_t::SGX_ERROR_INVALID_PARAMETER
+    }
+}
+
+/// FIXME: struct / typedef / fixed-size array for chain info and account?
+#[no_mangle]
+pub extern "C" fn ecall_check_withdraw_tx(
+    actual_fee_paid: *mut u64,
+    chain_info: *const u8,
+    chain_info_len: usize,
+    txaux: *const u8,
+    txaux_len: usize,
+    account: *const u8,
+    account_len: usize,
+) -> sgx_status_t {
+    let info = match check_chain_info(chain_info, chain_info_len) {
+        None => {
+            return sgx_status_t::SGX_ERROR_INVALID_PARAMETER;
+        }
+        Some(x) => x,
+    };
+
+    let mut txaux_slice = unsafe { slice::from_raw_parts(txaux, txaux_len) };
+    let txaux = TxAux::decode(&mut txaux_slice);
+    if let Some(TxAux::WithdrawUnbondedStakeTx {
+        txid,
+        no_of_outputs,
+        payload: TxObfuscated { txpayload, .. },
+        witness,
+        ..
+    }) = txaux
+    {
+        let address = verify_tx_recover_address(&witness, &txid);
+        if address.is_err() {
+            return sgx_status_t::SGX_ERROR_INVALID_PARAMETER;
+        }
+        // FIXME: decrypting
+        let mut account_slice = unsafe { slice::from_raw_parts(account, account_len) };
+        let account = StakedState::decode(&mut account_slice);
+        let plaintx = PlainTxAux::decode(&mut txpayload.as_slice());
+        match (plaintx, account) {
+            (Some(PlainTxAux::WithdrawUnbondedStakeTx(tx)), Some(account)) => {
+                if tx.id() != txid
+                    || no_of_outputs != tx.outputs.len() as TxoIndex
+                    || account.address != address.unwrap()
+                {
+                    return sgx_status_t::SGX_ERROR_INVALID_PARAMETER;
+                }
+                let result = verify_unbonded_withdraw_core(&tx, info, &account);
+                if result.is_err() {
+                    return sgx_status_t::SGX_ERROR_INVALID_PARAMETER;
+                }
+                let actual_fee: u64 = result.unwrap().to_coin().into();
+                unsafe {
+                    *actual_fee_paid = actual_fee;
+                }
+                // FIXME: sealing
+                sgx_status_t::SGX_SUCCESS
+            }
+            _ => {
+                return sgx_status_t::SGX_ERROR_INVALID_PARAMETER;
+            }
+        }
+    } else {
+        sgx_status_t::SGX_ERROR_INVALID_PARAMETER
+    }
 }
